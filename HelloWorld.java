36e248343a01c1d9971c1fd1c382ed9e0bd1284e
SpaceTracker <83755715+spacetracker-collab@users.noreply.github.com>
2021-06-16 20:44:18 +0530

    Snake Print
    How to snake print (or spiral print) a matrix

---
+++ HelloWorld.java
@@ -0,0 +1,35 @@
+public class HelloWorld{
+
+     public static void main(String []args){
+        System.out.println("Hello World Aradhana");
+        int matrix[][]={{1,2,3},{4,5,6},{7,8,9},{10,11,12}};
+     int row=4;
+     int column=3;
+     snakePrint(matrix,row,column);
+     }
+     
+     public static void snakePrint(int matrix[][],int row,int column)
+     {
+         
+         for(int i=0;i<matrix.length;i++)
+         {
+             
+             if(i%2==0)
+             {
+                 for(int j=0;j<matrix[0].length;j++)
+                 {
+                     System.out.println(matrix[i][j] + " ");
+                 }
+             }
+             else
+             {
+                 
+                 for(int j=matrix[0].length-1;j>=0;j--)
+                 {
+                     System.out.println(matrix[i][j] + " ");
+                 }
+             }
+         }
+     }
+     
+}
