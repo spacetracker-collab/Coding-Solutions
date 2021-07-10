c2ddc0007c3bf41eb4896e2aa2e2a697184f1d82
SpaceTracker <83755715+spacetracker-collab@users.noreply.github.com>
2021-07-10 19:10:55 +0530

    Create ComplexPolarForm.java

---
+++ ComplexPolarForm.java
@@ -0,0 +1,96 @@
+public class ComplexPolarForm {
+
+    public static void main(String[] args) {
+
+        //Real part>0
+        ComplexNumber complexRectangular = new ComplexNumber(10,3);
+        double[] polarCoardinates = complexRectangular.toPolar(complexRectangular);
+        System.out.println(polarCoardinates[0]+" (cos("+polarCoardinates[1]+") +i sin("+polarCoardinates[1]+"))");
+
+
+    }
+
+    public static ComplexNumber[][] addComplexMatrix(ComplexNumber[][] complexMatrixA, int complexMatrixA_rows,
+                                                     int complexMatrixA_cols, ComplexNumber[][] complexMatrixB, int complexMatrixB_rows, int complexMatrixB_col)
+            throws MatrixDimensionsError {
+
+        ComplexNumber sum[][] = new ComplexNumber[complexMatrixA.length][complexMatrixA[0].length];
+        if (complexMatrixA.length == complexMatrixB.length && complexMatrixA[0].length == complexMatrixB[0].length) {
+            for (int i = 0; i < complexMatrixA.length; i++) {
+                for (int j = 0; j < complexMatrixA[0].length; j++) {
+                    ComplexNumber complexi = complexMatrixA[i][j];
+                    ComplexNumber complexj = complexMatrixB[i][j];
+                    sum[i][j] = complexi.addNumber(complexj);
+                }
+            }
+        } else {
+            throw new MatrixDimensionsError("Rows and Col not equal");
+        }
+        return sum;
+
+    }
+
+}
+
+class MatrixDimensionsError extends Exception {
+    public MatrixDimensionsError(String s) {
+        // Call constructor of parent Exception
+        super(s);
+    }
+}
+
+class ComplexNumber {
+
+    double realNo = 0d;
+    double imgNo = 0d;
+
+    ComplexNumber(double realNo, double imgNo) {
+        this.realNo = realNo;
+        this.imgNo = imgNo;
+    }
+
+    public double getRealNo() {
+        return realNo;
+    }
+
+    public void setRealNo(double realNo) {
+        this.realNo = realNo;
+    }
+
+    public double getImgNo() {
+        return imgNo;
+    }
+
+    public void setImgNo(double imgNo) {
+        this.imgNo = imgNo;
+    }
+
+    // 3+2i
+    public void printMethod(ComplexNumber complex) {
+        System.out.println(String.valueOf(complex.getRealNo()) + "+" + String.valueOf(complex.getImgNo()) + "i");
+    }
+
+    public ComplexNumber addNumber(ComplexNumber c) {
+        double realAddResult = c.getRealNo() + this.realNo;
+        double imgResult = c.getImgNo() + this.imgNo;
+        return new ComplexNumber(realAddResult, imgResult);
+    }
+
+    public ComplexNumber multiNumber(ComplexNumber c) {
+        double real = this.realNo * c.realNo - this.imgNo * c.getImgNo();
+        double img = this.realNo * c.getImgNo() + this.getImgNo() * c.realNo;
+        return new ComplexNumber(real, img);
+
+    }
+
+    public double[] toPolar(ComplexNumber c) {
+        double realAddResult = c.getRealNo();
+        double imgResult = c.getImgNo();
+        double square = (realAddResult*realAddResult) +  (imgResult*imgResult);
+
+        double[] polarCoardinates = {Math.sqrt(square), Math.atan(imgResult/realAddResult)};
+
+        return polarCoardinates;
+    }
+}
+
